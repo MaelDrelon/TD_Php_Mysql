f55ddc449635a5fb517dac24dd66966c9c8ff5ba
MaelDrelon <mael.drelon1@gmail.com>
2020-11-26 10:25:54 +0100

    upd

--- Exo2.php
+++ Exo2.php
@@ -2,27 +2,28 @@
     try 
     {
         $maBase= new PDO('mysql:host=192.168.65.227; dbname=MaelDrelon; charset=utf8','mael', '');
+        $marequete = $maBase->query('SELECT * FROM Medecin');
+
+        function afficher_requet_select($marequete)
+        {
+            while ($table = $marequete ->fetch())
+            {
+                echo $table["Nom"]."".$table["Prenom"]."".$table["Matricule"];
+            }
+        verif();
+        }
+
+        function verif()
+        {
+            if($table["Matricule"]=false)
+            {
+                echo "<div style 'color=red;'> Resultat introuvable </div>";
+            }
+        }
     }
 
     catch (Exception $erreur)
     {
         die ('Erreur : '.$erreur ->getMessage());
-    }
-    $marequete = $maBase->query('SELECT * FROM Medecin');
-    function afficher_requet_select($marequete)
-    {
-        while ($table = $marequete ->fetch())
-        {
-            echo $table["Nom"]."".$table["Prenom"]."".$table["Matricule"];
-        }
-        verif();
-    }
-
-    function verif()
-    {
-        if($table["Matricule"]=false)
-        {
-            echo "<div style 'color=red;'> Resultat introuvable </div>";
-        }
     }
 ?>
