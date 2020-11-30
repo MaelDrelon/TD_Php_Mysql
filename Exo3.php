d7e90f37ff9450bad2f943d0d0bfbfba484eb14a
MaelDrelon <mael.drelon1@gmail.com>
2020-11-30 17:13:30 +0100

    upd

--- Exo3.php
+++ Exo3.php
@@ -2,24 +2,26 @@
     try 
     {
         $maBase= new PDO('mysql:host=192.168.65.227; dbname=MaelDrelon; charset=utf8','mael', '');
-      
-      if(isset($_POST['Matricule'],$_POST['Nom'],$_POST['Prenom'])) {
-        $req = $maBase->exec("INSERT INTO `Medecin`(`Matricule`, `Nom`, `Prenom`) VALUES('".$_POST['Matricule']."','".$_POST['Nom']."','".$_POST['Prenom']."')");
-        echo "INSERT INTO `Medecin`(`Matricule`, `Nom`, `Prenom`) VALUES('".$_POST['Matricule']."','".$_POST['Nom']."','".$_POST['Prenom']."')";
-        
-        //$req = $maBase->exec("INSERT INTO `Medecin`(`Matricule`, `Nom`, `Prenom`) VALUES('124654','bonjour','coucou')");
-      }
-
-/*
+        if(isset($_POST['Matricule'],$_POST['Nom'],$_POST['Prenom'])) 
+        {
+            if ((!empty($_POST['Matricule'])) && (!empty($_POST['Nom']))	&& (!empty($_POST['Prenom'])))
+            {
+                $req = $maBase->exec("INSERT INTO `Medecin`(`Matricule`, `Nom`, `Prenom`) VALUES('".$_POST['Matricule']."','".$_POST['Nom']."','".$_POST['Prenom']."')");
+                echo "INSERT INTO `Medecin`(`Matricule`, `Nom`, `Prenom`) VALUES('".$_POST['Matricule']."','".$_POST['Nom']."','".$_POST['Prenom']."')";
+                //$req = $maBase->exec("INSERT INTO `Medecin`(`Matricule`, `Nom`, `Prenom`) VALUES('124654','bonjour','coucou')");
+            }
+        }
+    
         function Insert($req)
         {
+            if(! $req)
 	        {
 	            die('impossible d’ajouter cet enregistrement : ');
 	        }
 	        echo "L’enregistrement est ajouté ";
         }
-        Insert($req);
-    */  }
+        
+    }
     catch (Exception $erreur)
     {
         die ('Erreur : '.$erreur ->getMessage());
@@ -27,14 +29,11 @@
 ?>
 <html>
 	<body>
-
-	<form action="Exo3.php" method="post">
-
-	Nom: <input type="text" name="Nom" />
-	Prénom: <input type="text" name="Prénom" />
-	Matricule: <input type="text" name="Matricule" />
-	<input type="submit" />
-	</form>
-
+	    <form action="" method="post">
+	        Nom: <input type="text" name="Nom" />
+	        Prénom: <input type="text" name="Prenom" />
+	        Matricule: <input type="text" name="Matricule" />
+	        <input type="submit" />
+	    </form>
 	</body>
-	</html>
+</html>
