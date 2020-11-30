5f4026eb6cc6c02368d12c25aa1cfdbf8c12cdc0
MaelDrelon <mael.drelon1@gmail.com>
2020-11-30 17:32:13 +0100

    upd

--- Exo3.php
+++ Exo3.php
@@ -1,4 +1,13 @@
 <?php
+
+    function Insert($req)
+    {
+        if(! $req)
+	    {
+	        die('impossible d’ajouter cet enregistrement : ');
+	    }
+	    echo "L’enregistrement est ajouté ";
+    }
     try 
     {
         $maBase= new PDO('mysql:host=192.168.65.227; dbname=MaelDrelon; charset=utf8','mael', '');
@@ -7,19 +16,11 @@
             if ((!empty($_POST['Matricule'])) && (!empty($_POST['Nom']))	&& (!empty($_POST['Prenom'])))
             {
                 $req = $maBase->exec("INSERT INTO `Medecin`(`Matricule`, `Nom`, `Prenom`) VALUES('".$_POST['Matricule']."','".$_POST['Nom']."','".$_POST['Prenom']."')");
-                echo "INSERT INTO `Medecin`(`Matricule`, `Nom`, `Prenom`) VALUES('".$_POST['Matricule']."','".$_POST['Nom']."','".$_POST['Prenom']."')";
+                echo '<p>'."INSERT INTO `Medecin`(`Matricule`, `Nom`, `Prenom`) VALUES('".$_POST['Matricule']."','".$_POST['Nom']."','".$_POST['Prenom']."')".'</p>';
                 //$req = $maBase->exec("INSERT INTO `Medecin`(`Matricule`, `Nom`, `Prenom`) VALUES('124654','bonjour','coucou')");
+                Insert($req);
             }
-        }
-    
-        function Insert($req)
-        {
-            if(! $req)
-	        {
-	            die('impossible d’ajouter cet enregistrement : ');
-	        }
-	        echo "L’enregistrement est ajouté ";
-        }
+        } 
     }
 
     catch (Exception $erreur)
