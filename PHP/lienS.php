1c75b4e986679bea0012c10dbf599664041c529d
MaelDrelon <mael.drelon1@gmail.com>
2020-12-03 16:37:57 +0100

    upd

--- PHP/lienS.php
+++ PHP/lienS.php
@@ -1,8 +1,27 @@
 <html>
 <head>
     <meta charset="utf-8">
+    <link rel="stylesheet" href="../menu.css">
 </head>
 <body>
+<p>
+        <nav>
+            <ul>
+                <li>
+                    <a href="../HTML.html">HTML</a>
+                </li>
+                <li>
+                    <a href="../CSS.html">CSS</a>
+                </li>
+                <li>
+                    <a href="../PHP.html">PHP</a>
+                </li>
+                <li>
+                    <a href="">Empty</a>
+                </li>
+                </ul>
+        </nav>
+    </p>
     <p>
     <?php
     echo "get distracted";
