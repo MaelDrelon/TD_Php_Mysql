bb9bd633db85baed41c8c77ae328c11f6546d396
MaelDrelon <mael.drelon1@gmail.com>
2020-12-10 12:33:54 +0100

    upd

--- PHP/PHP_Partie1_Exo1.php
+++ PHP/PHP_Partie1_Exo1.php
@@ -1,26 +1,72 @@
 <head>
 <link rel="stylesheet" type="text/css" href="exo1.css"> 
-<link rel="stylesheet" href="../menu.css">
+<link rel="stylesheet" href="../Index.css">
 </head>
 <body>
 <p>
         <nav>
-            <ul>
-                <li>
-                    <a href="../HTML.html">HTML</a>
-                </li>
-                <li>
-                    <a href="../CSS.html">CSS</a>
-                </li>
-                <li>
-                    <a href="../PHP.html">PHP</a>
-                </li>
-                <li>
-                    <a href="">Empty</a>
-                </li>
+          <ul>
+            <li class="deroulant"><a href="../PHP.php">PHP &ensp;</a>
+              <ul class="sous">
+                <li><a href="../PHPPart1.php">PHP Partie 1</a></li>
+                <li><a href="PHP_Partie1_Exo1.php">Exo 1</a></li>
+                <li><a href="PHP_Partie1_Exo2.php">Exo 2</a></li>
+                <li><a href="PHP_Partie1_Exo3.php">Exo 3</a></li>
+                <li><a href="PHP_Partie1_Exo4.php">Exo 4</a></li>
+                <li><a href="PHP_Partie1_Exo5.php">Exo 5</a></li>
+                <li><a href="PHP_Partie1_Exo6.php">Exo 6</a></li>
+                <li><a href="PHP_Partie1_Exo7.php">Exo 7</a></li>
+                <li><a href="PHP_Partie1_Exo8.php">Exo 8</a></li>
+                <li><a href="PHP_Partie1_TPFinal.php">Exo Final</a></li>
+                <li><a href="../PHPPart2.php">PHP Partie 2</a></li>
+                <li><a href="PHP_Partie2_Exo1.php">Exo 1</a></li>
+                <li><a href="PHP_Partie2_Exo2.php">Exo 2</a></li>
+                <li><a href="PHP_Partie2_Exo3.php">Exo 3</a></li>
+                <li><a href="PHP_Partie2_ExoFinal.php">Exo Final</a></li>
                 </ul>
+              </li>
+              <li class="deroulant"><a href="../BDD.php">BDD</a>
+                <ul class="sous">
+                  <li><a href="BDD_TD1_Exo1.php">TD1 Exo 1</a></li>
+                  <li><a href="BDD_TD1_Exo2.php">TD1 Exo 2</a></li>
+                  <li><a href="BDD_TD1_Exo3.php">TD1 Exo 3</a></li>
+                  <li><a href="BDD_TD1_Exo4.php">TD1 Exo 4</a></li>
+                  <li><a href="BDD_TD1_Exo5.php">TD1 Exo 5</a></li>
+                  <li><a href="BDD_TD1_Exo6.php">TD1 Exo 6</a></li>
+                  <li><a href="BDD_TD1_Exo7.php">TD1 Exo 7</a></li>
+                  <li><a href="BDD_TD2_Exo1.php">TD2 Exo 1</a></li>
+                  <li><a href="BDD_TD2_Exo2.php">TD2 Exo 2</a></li>
+                  <li><a href="BDD_TD2_Exo3.php">TD2 Exo 3</a></li>
+                </ul>
+              </li>
+              <li class="deroulant"><a href="../SQL.php">SQL</a>
+                <ul class="sous">
+                  <li><a href="SQL_TD3_Exo2.php">TD3 Exo 2</a></li>
+                  <li><a href="SQL_TD4_Exo1.php">TD4 Exo 1</a></li>
+                  <li><a href="SQL_TD4_Exo2.php">TD4 Exo 2</a></li>
+                  <li><a href="SQL_TD4_Exo3.php">TD4 Exo 3</a></li>
+                </ul>
+              </li>
+            <li class="deroulant"><a href="../CSS.php">CSS &ensp;</a>
+              <ul class="sous">
+                <li><a href="../CSS/CSSexo1.html">Exo 1</a></li>
+                <li><a href="../CSS/CSSexo2.html">Exo 2</a></li>
+                <li><a href="../CSS/CSSexo3.html">Exo 3</a></li>
+                <li><a href="../CSS/CSSexo4.html">Exo 4</a></li>
+              </ul>
+            </li>
+            <li class="deroulant"><a href="../HTML.php">HTML</a>
+              <ul class="sous">
+                <li><a href="../HTML/Exo1.html">Exo 1</a></li>
+                <li><a href="../HTML/Exo2.html">Exo 2</a></li>
+                <li><a href="../HTML/Exo3.html">Exo 3</a></li>
+                <li><a href="../HTML/Exo5.html">Exo 5</a></li>
+              </ul>
+            </li>
+            <li><a href="../index.php">Acceuil</a></li>
+          </ul>
         </nav>
-    </p>
+      </p>
     <?php
         $NombreAleatoire = rand ( 0 , 100 );
 
@@ -37,6 +83,6 @@
             echo '<div class="red">'.$NombreAleatoire. ' est pair</div>';
 
         }
-        highlight_file((__FILE__));
+        
     ?>  
 </body>
---
+++ index.php
@@ -0,0 +1,10 @@
+<?php
+include "Fonction.php";
+menu();
+?>
+<head>
+    <link rel="stylesheet" href="Index.css">
+</head>
+<body>
+    <div align=center>Bienvenue</div>
+</body>
